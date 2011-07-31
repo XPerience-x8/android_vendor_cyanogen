86a02d25f39647055d61beb744dcd57b52104145
Vassilis Tsogkas <tsogkas@ceid.upatras.gr>
2011-07-31 17:30:36 +0300

    Moving prelink file for x10mini to device/se/x10mini
    Change-Id: I7751c9c47a974b6f9a40ebcaf384dd316fb2e7bb

--- products/cyanogen_x10mini.mk
+++ products/cyanogen_x10mini.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := X10mini
 PRODUCT_MANUFACTURER := SE
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=x10mini BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GRJ22 BUILD_FINGERPRINT=google/passion/passion:2.3.4/GRJ22/121341:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.4 GRJ22 121341 release-keys"
 
-PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/cyanogen/prelink-linux-arm-x10mini.map
+PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/device/se/x10mini/prelink-linux-arm-x10mini.map
 
 # Add LDPI assets, in addition to MDPI
 PRODUCT_LOCALES += ldpi mdpi
