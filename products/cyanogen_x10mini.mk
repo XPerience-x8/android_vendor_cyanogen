d288dff09e833b16f32a9f0dea05da66f91ef341
Vassilis Tsogkas <tsogkas@ceid.upatras.gr>
2011-08-03 22:50:57 +0300

    MiniCM7-2.0.5 build
    Change-Id: Ib23ba204a9f7765dbaeae92fc103400ea60ebf18

--- products/cyanogen_x10mini.mk
+++ products/cyanogen_x10mini.mk
@@ -15,7 +15,7 @@ PRODUCT_BRAND := SE
 PRODUCT_DEVICE := x10mini
 PRODUCT_MODEL := X10mini
 PRODUCT_MANUFACTURER := SE
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=x10mini BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GRJ22 BUILD_FINGERPRINT=google/passion/passion:2.3.4/GRJ22/121341:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.4 GRJ22 121341 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=x10mini BUILD_ID=GRJ90 BUILD_DISPLAY_ID=GRJ90 BUILD_FINGERPRINT=google/passion/passion:2.3.4/GRJ22/121341:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.4 GRJ22 121341 release-keys"
 
 PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/device/se/x10mini/prelink-linux-arm-x10mini.map
 
@@ -38,7 +38,7 @@ PRODUCT_PACKAGES += CMWallpapers
 # Set ro.modversion
 #
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.modversion=MiniCM7-2.0.4
+	ro.modversion=MiniCM7-2.0.5
 
 #
 # Set ro.modversion
