7c6f3209fdfc27cae41521ef93fe38115ab5b9b1
nobodyAtall <tsogkas@ceid.upatras.gr>
2011-10-29 12:21:57 +0300

    Renaming x10mini to robyn
    Change-Id: Ie445798cd019d31168d8b43c4ecfcd432570dfd4

--- products/cyanogen_robyn.mk
+++ products/cyanogen_robyn.mk
@@ -17,6 +17,8 @@ PRODUCT_MODEL := E10i
 PRODUCT_MANUFACTURER := Sony Ericsson
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE=E10i PRODUCT_NAME=E10i BUILD_ID=3.0.1.A.0.145 BUILD_FINGERPRINT=SEMC/LT15i_1247-1073/LT15i:2.3.3/3.0.1.A.0.145/bn_p:user/release-keys PRIVATE_BUILD_DESC="LT15i-user 2.3.3 3.0.1.A.0.145 bn_P test-keys"
 
+PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/device/semc/robyn/prelink-linux-arm-robyn.map
+
 # Add the Torch app
 PRODUCT_PACKAGES += Torch
 
@@ -26,16 +28,40 @@ PRODUCT_LOCALES += ldpi mdpi
 # Extra overlay for LDPI
 PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/ldpi
 
-# Ti FM radio
-#$(call inherit-product, vendor/cyanogen/products/ti_fm_radio.mk)
+# Enable Windows Media
+WITH_WINDOWS_MEDIA := true
 
-# Release name and versioning
-PRODUCT_RELEASE_NAME := X10mini
-PRODUCT_VERSION_DEVICE_SPECIFIC :=
--include vendor/cyanogen/products/common_versions.mk
+# Add the Torch app
+PRODUCT_PACKAGES += Torch
+
+# Add the Cyanogenmod Wallpapers
+PRODUCT_PACKAGES += CMWallpapers
 
 #
-# Copy MDPI specific prebuilt files
+# Set ro.modversion
+#
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.modversion=MiniCM7-2.0.6
+
+#
+# Set ro.modversion
+#
+#ifdef CYANOGEN_NIGHTLY
+#    PRODUCT_PROPERTY_OVERRIDES += \
+#        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-x10Mini
+#else
+#    ifdef CYANOGEN_RELEASE
+#        PRODUCT_PROPERTY_OVERRIDES += \
+#            ro.modversion=CyanogenMod-7.0.2-X10Mini
+#    else
+#        PRODUCT_PROPERTY_OVERRIDES += \
+#            ro.modversion=CyanogenMod-7.0.2-X10Mini-000
+#    endif
+#endif
+
+#
+# Copy bootanimation
 #
 PRODUCT_COPY_FILES +=  \
     vendor/cyanogen/prebuilt/ldpi/media/bootanimation.zip:system/media/bootanimation.zip
+
