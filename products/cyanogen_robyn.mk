21e197072e9da2e7dc57ab40be3682a1b42ae9c4
nobodyAtall <tsogkas@ceid.upatras.gr>
2011-12-21 11:21:11 +0200

    MiniCM7-2.1.7
    Change-Id: Id49159752baedb7442c8f3f3f5f8adb12e5f2511

--- products/cyanogen_robyn.mk
+++ products/cyanogen_robyn.mk
@@ -15,13 +15,10 @@ PRODUCT_BRAND := SEMC
 PRODUCT_DEVICE := robyn
 PRODUCT_MODEL := E10i
 PRODUCT_MANUFACTURER := Sony Ericsson
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE=E10i PRODUCT_NAME=E10i BUILD_ID=3.0.1.A.0.145 BUILD_FINGERPRINT=SEMC/LT15i_1247-1073/LT15i:2.3.3/3.0.1.A.0.145/bn_p:user/release-keys PRIVATE_BUILD_DESC="LT15i-user 2.3.3 3.0.1.A.0.145 bn_P test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE=E10i PRODUCT_NAME=E10i BUILD_ID=3.0.1.A.0.145 BUILD_DISPLAY_ID=GWK74 BUILD_FINGERPRINT=SEMC/LT15i_1247-1073/LT15i:2.3.3/3.0.1.A.0.145/bn_p:user/release-keys PRIVATE_BUILD_DESC="LT15i-user 2.3.3 3.0.1.A.0.145 bn_P test-keys"
 
-PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/device/semc/robyn/prelink-linux-arm-robyn.map
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=GWK74
-
-# Add the Torch app
-PRODUCT_PACKAGES += Torch
+# Ti FM radio
+#$(call inherit-product, vendor/cyanogen/products/ti_fm_radio.mk)
 
 # Add LDPI assets, in addition to MDPI
 PRODUCT_LOCALES += ldpi mdpi
@@ -29,43 +26,17 @@ PRODUCT_LOCALES += ldpi mdpi
 # Extra overlay for LDPI
 PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/ldpi
 
-# Ti FM radio
-#$(call inherit-product, vendor/cyanogen/products/ti_fm_radio.mk)
-
 # Enable Windows Media
 WITH_WINDOWS_MEDIA := true
-
-# Add the Torch app
-PRODUCT_PACKAGES += Torch
-
-# Add the Cyanogenmod Wallpapers
-PRODUCT_PACKAGES += CMWallpapers
 
 #
 # Set ro.modversion
 #
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.modversion=MiniCM7-2.1.6
-
-#
-# Set ro.modversion
-#
-#ifdef CYANOGEN_NIGHTLY
-#    PRODUCT_PROPERTY_OVERRIDES += \
-#        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-x10Mini
-#else
-#    ifdef CYANOGEN_RELEASE
-#        PRODUCT_PROPERTY_OVERRIDES += \
-#            ro.modversion=CyanogenMod-7.0.2-X10Mini
-#    else
-#        PRODUCT_PROPERTY_OVERRIDES += \
-#            ro.modversion=CyanogenMod-7.0.2-X10Mini-000
-#    endif
-#endif
+	ro.modversion=MiniCM7-2.1.7
 
 #
 # Copy bootanimation
 #
 PRODUCT_COPY_FILES +=  \
     vendor/cyanogen/prebuilt/ldpi/media/bootanimation.zip:system/media/bootanimation.zip
-
